0d183b7b7b52b216692db1e3314a63cbb2bb5e02
Yan Zhang <yan.zhang13@mail.mcgill.ca>
2023-04-28 03:12:49 -0400

    test: analyze results for each algorithm
     - Run experiments for each algorithm and plot 3 figures in paper
     - Bug #3 is fixed

--- predator_prey/dqn_so_aba.py
+++ predator_prey/dqn_so_aba.py
@@ -14,16 +14,17 @@ if __name__ == '__main__':
     prs.add_argument("-a", dest="alpha", type=float, default=0.001, required=False, help="Alpha learning rate.\n")
     prs.add_argument("-b", dest="buffer", type=int, default=50000, required=False, help="Buffer size of DQN.\n")
     prs.add_argument("-ef", dest="exploration_f", type=float, default=0.1, required=False, help="exploration_fraction.\n")
-    prs.add_argument("-efs", dest="exploration_f_e", type=float, default=0.02, required=False, help="exploration_final_eps.\n")
-    prs.add_argument("-batch", dest="batch_size", type=int, default=64, required=False, help="Batch size for NN.\n")
+    prs.add_argument("-efs", dest="exploration_f_e", type=float, default=0.1, required=False, help="exploration_final_eps.\n")
+    prs.add_argument("-batch", dest="batch_size", type=int, default=256, required=False, help="Batch size for NN.\n")
     prs.add_argument("-targfr", dest="target_net_freq", type=int, default=500, required=False, help="Update freq for target.\n")
     prs.add_argument("-fr", dest="ifr", type=int, default=2, required=False, help="Functional Response for SC\n")
     prs.add_argument("-fnum", dest="ifrnum", type=int, default=2, required=False, help="Functional Response Num for SC\n")
     prs.add_argument("-w", dest="weight", type=int, default=2, required=False, help="Weight coefficient\n")
-    prs.add_argument("-ggi", action="store_false", default=False, help="Run GGI algo or not.\n")
+    prs.add_argument("-ggi", action="store_true", default=False, help="Run GGI algo or not.\n")
+    prs.add_argument("-id", dest="run_index", type=int, default=0, help="Run index.\n")
     args = prs.parse_args()
     ggi = args.ggi
-    env = PredatorPrey(out_csv_name='results/reward', ggi=ggi, iFR=args.ifr, iFRnum=args.ifrnum)
+    env = PredatorPrey(out_csv_name='results/reward_dqn' + ('ggi' if ggi else '') + str(args.run_index), ggi=ggi, iFR=args.ifr, iFRnum=args.ifrnum)
     weight_coef = args.weight
     
     if ggi:
@@ -39,7 +40,8 @@ if __name__ == '__main__':
             exploration_final_eps=args.exploration_f_e,
             batch_size=args.batch_size,
             double_q=False,
-            target_network_update_freq = args.target_net_freq
+            target_network_update_freq = args.target_net_freq,
+            n_cpu_tf_sess = 20
         )
     else:
         from stable_baselines.deepq import DQN, MlpPolicy
@@ -52,7 +54,8 @@ if __name__ == '__main__':
             exploration_final_eps=args.exploration_f_e,
             batch_size=args.batch_size,
             double_q=False,
-            target_network_update_freq = args.target_net_freq
+            target_network_update_freq = args.target_net_freq,
+            n_cpu_tf_sess = 20
         )
     # learning of the model
     model.learn(total_timesteps=100000)
--- predator_prey/random_so_aba.py
+++ predator_prey/random_so_aba.py
@@ -10,11 +10,11 @@ if __name__ == '__main__':
                                   description=""" Random """)
     prs.add_argument("-fr", dest="ifr", type=int, default=2, required=False, help="Functional Response for SC\n")
     prs.add_argument("-fnum", dest="ifrnum", type=int, default=2, required=False, help="Functional Response Num for SC\n")
-
+    prs.add_argument("-id", dest="run_index", type=int, default=0, help="Run index.\n")
     args = prs.parse_args()
 
     ggi = False
-    env = PredatorPrey(out_csv_name='results/reward_random', ggi=ggi, iFR=args.ifr, iFRnum=args.ifrnum)
+    env = PredatorPrey(out_csv_name=f'results/reward_random{args.run_index}', ggi=ggi, iFR=args.ifr, iFRnum=args.ifrnum)
     obs = env.reset()
     for i in range(100000):
         action = env.action_space.sample()
